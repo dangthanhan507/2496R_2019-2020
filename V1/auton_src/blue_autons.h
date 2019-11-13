382c76990c6f3300e3142f751e5f1a764bb119c6
Ravdrew <53586118+Ravdrew@users.noreply.github.com>
2019-11-12 16:47:23 -0800

    Update blue_autons.h

--- V1/auton_src/blue_autons.h
+++ V1/auton_src/blue_autons.h
@@ -38,7 +38,7 @@ void top_blue_tower() {
 }
 
 void skills() {
-  device_setup();
+  /*device_setup();
   //deploy()
   stop_chas(brakeType::hold);
   thread t1(lift_claw_subsystem);
@@ -77,7 +77,7 @@ void skills() {
 
   fwd_1D(-1100,-1000,-1000);
   lift_target = lift_min;
-  /*
+  
   Gyro.startCalibration();
   sleepMs(2000);
   turn_1D(-62,-60,-60);
@@ -103,6 +103,33 @@ void skills() {
 
 
   //lift();
+
+  device_setup();
+  stop_chas(brakeType::hold);
+  thread t1(lift_claw_subsystem);
+  lift_pid_status = true;
+  lift_target = lift_min;
+  fwd_1D(1010,1500,1500);
+  deploy();
+  sleepMs(500);
+  fwd_1D(-25,-400,-600);
+  turn_1D(62,50,60);
+  sleepMs(200);
+  fwd_1D(440,800,1000);
+  grab = true;
+  sleepMs(200);
+  lift_target = lift_min - 1250;
+  sleepMs(1500);
+  fwd_1D(600,800,2000);
+  sleepMs(200);
+  grab = false;//first tower
+  fwd_1D(-60,-500,-800);
+  lift_target = lift_min;
+  turn_1D(-60,-50,-60);
+  fwd_1D(600,800,1000);
+  turn_1D(-30,-50,-60);
+  fwd_1D(1000,600,800);
+  lift();
 }
 
 
